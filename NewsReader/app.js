2f04a49db5aac649e9d6a2212083205d43d74164
Rachana Acharya <rasnacharya@gmail.com>
2015-09-25 00:16:48 -0600

    Added key press n to move to next article

--- NewsReader/app.js
+++ NewsReader/app.js
@@ -10,7 +10,16 @@ var main = function() {
   $(document).keypress(function(event) {
     if (event.which === 111) {
       $('.current').children('.description').toggle();
+    } else if (event.which === 110) {
+      var currentArticle = $('.current');
+      var nextArticle = currentArticle.next();
+
+      currentArticle.removeClass('current');
+      nextArticle.addClass('current');
     };
   });
+
+
+
 };
 $(document).ready(main);
