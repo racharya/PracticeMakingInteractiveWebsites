5190db96174d175f34fb20de4f60fe760e96a0a7
Rachana Acharya <rasnacharya@gmail.com>
2015-09-24 23:55:53 -0600

    Added keyboard handler to toggle current article description when 'o' is pressed

--- NewsReader/app.js
+++ NewsReader/app.js
@@ -1,12 +1,16 @@
 var main = function() {
-    $('.article').click(function() {
-      $('.article').removeClass('current');
-      $('.description').hide();
+  $('.article').click(function() {
+    $('.article').removeClass('current');
+    $('.description').hide();
 
-      $(this).addClass('current');
-      $(this).children('.description').show();
-    });
+    $(this).addClass('current');
+    $(this).children('.description').show();
+  });
 
-    $(document).keypress(function(event) {
-
-        }; $(document).ready(main);
+  $(document).keypress(function(event) {
+    if (event.which === 111) {
+      $('.current').children('.description').toggle();
+    };
+  });
+};
+$(document).ready(main);
