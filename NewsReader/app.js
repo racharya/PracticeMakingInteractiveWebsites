2659948311069e8f57e0ada8ed749757d050e290
Rachana Acharya <rasnacharya@gmail.com>
2015-09-24 23:38:59 -0600

    Added logic to mark the current article gray

--- NewsReader/app.js
+++ NewsReader/app.js
@@ -1,6 +1,9 @@
 var main = function() {
   $('.article').click(function() {
+    $('.article').removeClass('current');
     $('.description').hide();
+
+    $(this).addClass('current');
     $(this).children('.description').show();
   });
 
