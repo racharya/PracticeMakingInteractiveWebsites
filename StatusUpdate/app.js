574aa9da0f4166f6691990b67bd1b0b614f1eb00
Rachana Acharya <rasnacharya@gmail.com>
2015-09-28 11:11:40 -0600

    Cleared out the contents in the status-box after it is posted

--- StatusUpdate/app.js
+++ StatusUpdate/app.js
@@ -2,6 +2,7 @@ var main = function() {
   $('.btn').click(function() {
     var post = $('.status-box').val();
     $('<li>').text(post).prependTo('.posts');
+    $('.status-box').val('');
   });
 }
 
