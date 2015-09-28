be2385aa1d5b272f94674dd7bc6bf7292216c732
Rachana Acharya <rasnacharya@gmail.com>
2015-09-28 11:16:23 -0600

    Added code to update character counter

--- StatusUpdate/app.js
+++ StatusUpdate/app.js
@@ -4,6 +4,11 @@ var main = function() {
     $('<li>').text(post).prependTo('.posts');
     $('.status-box').val('');
   });
+  $('.status-box').keyup(function() {
+    var postLength = $(this).val().length;
+    var charactersLeft = 140 - postLength;
+    $('.counter').text(charactersLeft);
+  });
 }
 
 $(document).ready(main);
