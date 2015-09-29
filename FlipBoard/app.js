6407607e5b6aeca90e51211b9871b4d6095f1766
Rachana Acharya <rasnacharya@gmail.com>
2015-09-28 21:07:31 -0600

    Added logic to fadein next slide

--- FlipBoard/app.js
+++ FlipBoard/app.js
@@ -2,6 +2,15 @@ var main = function() {
   $('.dropdown-toggle').click(function() {
     $('.dropdown-menu').toggle();
   });
+  $('.arrow-next').click(function() {
+    var currentSlide = $('.active-slide');
+    var nextSlide = currentSlide.next();
 
+    currentSlide.fadeOut(600);
+    currentSlide.removeClass('active-slide');
+
+    nextSlide.fadeIn(600);
+    nextSlide.addClass('active-slide');
+  });
 };
 $(document).ready(main);
