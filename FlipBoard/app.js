71790abec0f67a550a7385a5a2cbf749fe091403
Rachana Acharya <rasnacharya@gmail.com>
2015-09-28 21:22:49 -0600

    Added logic to update dot when transitioned into the next slide

--- FlipBoard/app.js
+++ FlipBoard/app.js
@@ -2,20 +2,26 @@ var main = function() {
   $('.dropdown-toggle').click(function() {
     $('.dropdown-menu').toggle();
   });
+
   $('.arrow-next').click(function() {
     var currentSlide = $('.active-slide');
     var nextSlide = currentSlide.next();
 
+    var currentDot = $('.active-dot');
+    var nextDot = currentDot.next();
+
     if (nextSlide.length === 0) {
       nextSlide = $('.slide').first();
+
     };
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
 
     currentSlide.fadeOut(600);
     currentSlide.removeClass('active-slide');
 
     nextSlide.fadeIn(600);
     nextSlide.addClass('active-slide');
-
 
   });
 };
