7a56cfdf3005eccfa33fda513ad1b4cc907ee725
Rachana Acharya <rasnacharya@gmail.com>
2015-09-28 21:16:23 -0600

    Added logic to go back to first slide when clicked on the arrow after the last slide

--- FlipBoard/app.js
+++ FlipBoard/app.js
@@ -6,11 +6,17 @@ var main = function() {
     var currentSlide = $('.active-slide');
     var nextSlide = currentSlide.next();
 
+    if (nextSlide.length === 0) {
+      nextSlide = $('.slide').first();
+    };
+
     currentSlide.fadeOut(600);
     currentSlide.removeClass('active-slide');
 
     nextSlide.fadeIn(600);
     nextSlide.addClass('active-slide');
+
+
   });
 };
 $(document).ready(main);
